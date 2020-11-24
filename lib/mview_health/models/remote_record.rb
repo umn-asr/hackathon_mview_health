370ef080a38491b118af825599e9047f28ca7b2c
Tony Thomas <thoma127@umn.edu>
2020-11-24 14:01:59 -0600

    Implement table_name method

--- lib/mview_health/models/remote_record.rb
+++ lib/mview_health/models/remote_record.rb
@@ -3,5 +3,23 @@ require "active_record"
 module MviewHealth
   class RemoteRecord < ActiveRecord::Base
     self.abstract_class = true
+
+    def self.table_name
+      view_or_table
+    end
+
+    class << self
+      private
+
+      attr_accessor :view_name
+
+      def view_or_table
+        if Rails.configuration.read_only_peoplesoft
+          view_name
+        else
+          "test_#{view_name}"
+        end
+      end
+    end
   end
 end
--- spec/models/remote_record_spec.rb
+++ spec/models/remote_record_spec.rb
@@ -1,14 +1,48 @@
 require "spec_helper"
+require "ostruct"
 require_relative "../../lib/mview_health/models/remote_record"
 
 RSpec.describe MviewHealth::RemoteRecord do
-  before :all do
-    # stub using Rails
-    module Rails
-    end
-  end
-
   it "is an abstract class" do
     expect(described_class.abstract_class).to be true
+  end
+
+  describe "concrete implementations" do
+    # test class that works
+    class ConcreteRemoteRecord < MviewHealth::RemoteRecord
+      self.view_name = "test"
+    end
+
+    describe "#table_name" do
+      context "when Rails.configuration.read_only_peoplesoft is true" do
+        before do
+          # stub using Rails
+          module Rails
+            def self.configuration
+              OpenStruct.new(read_only_peoplesoft: true)
+            end
+          end
+        end
+
+        it "uses the view_name" do
+          expect(ConcreteRemoteRecord.table_name).to eq("test")
+        end
+      end
+
+      context "when Rails.configuration.read_only_peoplesoft is false" do
+        before do
+          # stub using Rails
+          module Rails
+            def self.configuration
+              OpenStruct.new(read_only_peoplesoft: false)
+            end
+          end
+        end
+
+        it "prefixes the view name with test" do
+          expect(ConcreteRemoteRecord.table_name).to eq("test_test")
+        end
+      end
+    end
   end
 end
