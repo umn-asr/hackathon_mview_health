173e1b0e181cefe5ea1a852ff356c57258754d5c
Tony Thomas <thoma127@umn.edu>
2020-11-25 10:37:30 -0600

    WIP: view_builder setup
    I had to set a couple of env vars manually for this to work, but it
    establishes a DB connection and the tests pass without error.
    
    Manual ENV vars:
    
    ```sh
    export DATABASE_FILE="/Users/thoma127/Projects/mview_health/spec/support/database.yml"
    export ENV="test"
    ```
    
    Add these to `script/setup`? Set these up in a different way?

--- lib/data_views/active_record_setup.rb
+++ lib/data_views/active_record_setup.rb
@@ -1,9 +1,10 @@
 require "active_record"
+require_relative "../mview_health"
 include ActiveRecord::Tasks
 
 DatabaseTasks.root = DataViews.root
 DatabaseTasks.env  = DataViews.environment
 
-DatabaseTasks.database_configuration = YAML.load(File.read(File.join(DatabaseTasks.root, "config", "database.yml")))
+DatabaseTasks.database_configuration = YAML.load(File.read(MviewHealth.config.database_file))
 ActiveRecord::Base.configurations = DatabaseTasks.database_configuration
-ActiveRecord::Base.establish_connection DatabaseTasks.env.to_sym
+ActiveRecord::Base.establish_connection MviewHealth.config.env.to_sym
