b0cc225e8436fb5900ecbaeb5cf0bdee481787ee
Tony Thomas <thoma127@umn.edu>
2020-11-25 10:51:44 -0600

    WIP: make the mview actually query mview health

--- lib/data_views/mview_metadata.rb
+++ lib/data_views/mview_metadata.rb
@@ -1,7 +1,20 @@
+# Module to hold all view_builder views
 module DataViews
+  # View of 'unusable' and 'last_refresh_time' for mviews used to evaluate their health
   class MviewMetadata
     def self.view_name
       "mview_metadata"
+    end
+
+    def self.definition_sql
+      <<~SQL
+        SELECT
+          mview_name as name,
+          last_refresh_date,
+          unusable
+        FROM
+          user_mview_analysis
+      SQL
     end
   end
 end
